c73eaf51528e4fd354fe7ad1c4b27f99c4c98420
Touhidansar <145832763+Touhidansar@users.noreply.github.com>
2023-10-03 23:10:37 +0530

    function

---
+++ _function.js
@@ -0,0 +1,17 @@
+
+
+function sayMyName(){
+    console.log("T")
+    console.log("O")
+    console.log("U")
+    console.log("H")
+    console.log("I")
+    console.log("D")
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers(5, "2")
